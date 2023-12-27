9d1c6dea7c6ec5f9ae371bc91602a12a1e24bec6
oscar <ofter3132@gmail.com>
2023-12-27 13:38:50 -0600

    cambio de nombre

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['us'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/login.css">
     <link rel="shortcut icon" href="assets/img/OXILIVE.ico" type="image/x-icon">
-    <title>Login</title>
+    <title>iniciar sesion</title>
 </head>
 
 <body>
