fc869a32f39400be8253ba96043e9707f5695209
oscar <ofter3132@gmail.com>
2023-12-27 10:27:10 -0600

    cambiooooooooooooooooooooooooooooooooo

--- config/session.php
+++ config/session.php
@@ -1,8 +1,12 @@
 <?php
-$url_base = 'http://localhost:8080/OXILIVE2.0/';
+$url_base = 'https://prueba.oxilive.com.mx/';
+$url_base = 'http://localhost:8080/OXILIVE/';
 session_start();
 if (isset($_SESSION["us"])) {
     include 'baseDatos.php';
+    ?>    
+    <h1>Bienvenido<?php  echo $_SESSION['us']?></h1>
+    <?php
 } else {
     header('location: '. $url_base .'login.php');
 }
--- procesarLogin.php
+++ procesarLogin.php
@@ -1,40 +1,58 @@
 <?php
-include("config/baseDatos.php");
+include_once("config/baseDatos.php");
 session_start(); // Asegúrate de iniciar la sesión
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
     $usuario = $_POST['username'];
     $contraseña = $_POST['password'];
-    
-    $sql = "SELECT * FROM usuarios WHERE Usuario = :usuario";
-    $stmt = $con->prepare($sql);
-    $stmt->bindParam(':usuario', $usuario);
-    $stmt->execute();
-    $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    if (count($resultado) > 0) {
-        foreach ($resultado as $filas) {
-            if ($resultado && password_verify($contraseña, $filas['paswword'])) {
-                $_SESSION['us'] = $filas['Usuario'];
-                $_SESSION['id'] = $filas['id_usuarios'];
-                header('location: index.php');
-                exit(); // Es importante detener la ejecución después de redirigir
-            } else {
+    try {
+
+        $stmt = $con->prepare("SELECT * FROM usuarios");
+        $stmt->execute();
+        $datos = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (count($datos) > 0) {
+            if ($datos && password_verify($contraseña, $datos["paswword"])) {
+                $_SESSION['id'] = $datos['id_usuarios'];
+                $_SESSION['us'] = $datos["Usuario"];
+
 ?>
+
                 <script>
-                    alert('error');
-                    window.location.href = "login.php";
+                    alert("bienvenido");
+                    window.location = 'index.php';
                 </script>
-        <?php
+
+            <?php
+            } else {
+            ?>
+
+                <script>
+                    alert("error en la contraseña");
+                    window.location = 'index.php';
+                </script>
+            <?php
+                echo "error en la contraseña";
             }
+        } else {
+            ?>
+            <script>
+                alert("Error en el usuario");
+                window.location = 'index.php';
+            </script>
+    <?php
         }
-    } else {
-        ?>
-        <script>
-            alert('error de usuario');
-            window.location.href = "login.php";
-        </script>
-<?php
+    } catch (Exception $e) {
+        echo "ERROR encontrado <br>";
     }
+} else {
+    ?>
+    <script>
+        alert("Error en el sistema");
+        window.location = 'index.php';
+    </script>
+<?php
 }
 ?>
