0c0c298c73574dc17a61f7caddbcb4cb268e3bda
oscar <ofter3132@gmail.com>
2023-12-27 16:48:53 -0600

    buscador rico

--- secciones/chat/php/usuarios.php
+++ secciones/chat/php/usuarios.php
@@ -5,8 +5,16 @@ include('../../../config/baseDatos.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $usuario = $_POST['buscar'];
 
-    $sql = "SELECT * FROM usuarios 
-    WHERE (Nombres LIKE '%$usuario%'
+    // $sql = "SELECT * FROM usuarios 
+    // WHERE (Nombres LIKE '%$usuario%'
+    // OR Apellidos LIKE '%$usuario%' 
+    // OR Usuario LIKE '%$usuario%') 
+    // AND id_usuarios <> $id_sesionActual;";
+
+    $sql = "SELECT Nombres, Apellidos, Usuario FROM usuarios 
+    WHERE CONCAT(Nombres, ' ', Apellidos) LIKE '%$usuario%' 
+    OR CONCAT(Apellidos, ' ' ,Nombres) LIKE '%$usuario%' 
+    OR (Nombres LIKE '%$usuario%'
     OR Apellidos LIKE '%$usuario%' 
     OR Usuario LIKE '%$usuario%') 
     AND id_usuarios <> $id_sesionActual;";
@@ -16,6 +24,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     foreach ($resultado as $key => $value) {
-        echo '<li data-id="'. $value['token'] .'">' . $value['Usuario'] . ': ' . $value['Nombres'] . ' ' . $value['Apellidos'] . '</li>';
+        echo '<li data-id="' . $value['token'] . '">' . $value['Usuario'] . ': ' . $value['Nombres'] . ' ' . $value['Apellidos'] . '</li>';
     }
 }
