291769accd9d830f7ea58a9769197bf747b310d5
oscar <ofter3132@gmail.com>
2024-01-05 12:33:26 -0600

    mmd

--- config/session.php
+++ config/session.php
@@ -5,7 +5,7 @@ session_start();
 if (isset($_SESSION["us"])) {
     include 'baseDatos.php';
     ?>    
-    <h1>Bienvenido <?php  echo $_SESSION['us']?></h1>
+    <h1>Bienvenido <?php  echo $_SESSION['us']; ?></h1>
     <?php
 } else {
     header('location: '. $url_base .'login.php');
